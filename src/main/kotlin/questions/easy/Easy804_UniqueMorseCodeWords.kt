a010fd63af2922f12a682cb2d38c19e7b943d188
Alex Talledo <alexta.dev@gmail.com>
2020-12-14 12:10:54 +0100

    Solution for question 804

---
+++ src/main/kotlin/questions/easy/Easy804_UniqueMorseCodeWords.kt
@@ -0,0 +1,55 @@
+package questions.easy
+
+fun main() {
+    val words = arrayOf("gin", "zen", "gig", "msg")
+    println(uniqueMorseRepresentations(words))
+}
+
+// Time complexity: O(mn), being m the amount of words and n the largest word
+// Space complexity: O(n), being n the amount of words
+fun uniqueMorseRepresentations(words: Array<String>): Int {
+    val unique = hashSetOf<String>()
+    for (word in words) {
+        val sb = StringBuilder()
+        for (c in word) {
+            sb.append(getMorseCode(c))
+        }
+        unique.add(sb.toString())
+    }
+    return unique.size
+}
+
+fun getMorseCode(c: Char): String {
+    val morseMapping = arrayOf(
+        ".-",
+        "-...",
+        "-.-.",
+        "-..",
+        ".",
+        "..-.",
+        "--.",
+        "....",
+        "..",
+        ".---",
+        "-.-",
+        ".-..",
+        "--",
+        "-.",
+        "---",
+        ".--.",
+        "--.-",
+        ".-.",
+        "...",
+        "-",
+        "..-",
+        "...-",
+        ".--",
+        "-..-",
+        "-.--",
+        "--.."
+    )
+    return morseMapping[getIndex(c)]
+}
+
+fun getIndex(c: Char) = c.minus('a')
+
