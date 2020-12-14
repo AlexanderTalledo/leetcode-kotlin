3904ff199e2872b8444a9284ce6bb563933c386c
Alex Talledo <alexta.dev@gmail.com>
2020-12-14 16:14:43 +0100

    Solution for question 1572

---
+++ src/main/kotlin/questions/easy/Easy1572_MatrixDiagonalSum.kt
@@ -0,0 +1,34 @@
+package questions.easy
+
+fun main() {
+    val mat = arrayOf(
+        intArrayOf(1, 2, 3),
+        intArrayOf(4, 5, 6),
+        intArrayOf(7, 8, 9)
+    )
+    println(diagonalSum(mat))
+}
+
+// Time complexity: O(n), being n the number of rows or columns
+// Space complexity: O(1), no extra space required
+fun diagonalSum(mat: Array<IntArray>): Int {
+    var sum = 0
+    var row = 0
+    var col = 0
+    while (row < mat.size) {
+        sum += mat[row++][col++]
+    }
+    row = 0
+    col = mat.size.minus(1)
+    while (col >= 0) {
+        if (col == row) {
+            ++row
+            --col
+            continue
+        }
+        sum += mat[row++][col--]
+    }
+    return sum
+}
+
+
